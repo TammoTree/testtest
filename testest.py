05f7d038c27f451d2def8be278d43e87b6e92067
TammoTree <91952536+TammoTree@users.noreply.github.com>
2021-12-14 07:58:21 +0100

    Version 1

---
+++ testest.py
@@ -0,0 +1,12 @@
+liste1 = []
+liste2 = []
+liste3 = []
+test1 = 1
+test2 = 2 
+
+for i in range(0,5):
+    liste1.append(test1)
+    liste2.append(test2)
+print(liste1, liste2)
+liste3 = liste1 + liste2
+print(liste3)
